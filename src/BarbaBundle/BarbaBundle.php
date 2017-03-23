54e1f246f9ffb4acc5ecef24e6b7a53faddc22d0
Bruno <brvalley@hotmail.com>
2017-03-23 20:25:11 -0300

    commit inicial!

---
+++ src/BarbaBundle/BarbaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BarbaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BarbaBundle extends Bundle
+{
+}
---
+++ src/BarbaBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BarbaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BarbaBundle:Default:index.html.twig');
+    }
+}
